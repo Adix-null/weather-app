06daa5d5fd47c85cd9e1c745df5fecfd2b1be5a0
Adix-null <adomas@stalas.lt>
2025-03-26 10:57:13 +0200

    Initialize app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+function App()
+{
+    return (
+        <div className="container">
+            <h1>Hello Weather</h1>
+            <p>Time to do this</p>
+        </div>
+    );
+}
+
+export default App;
